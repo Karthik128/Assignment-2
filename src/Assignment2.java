e02bab09524ef7a308832da900bb94bf6653aa3f
Karthik128 <dr.m.karthiik@gmail.com>
2016-04-19 18:07:54 +0530

    Assignment2

---
+++ src/Assignment2.java
@@ -0,0 +1,12 @@
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Integer a,b=4,c;
+    Integer d=b;
+    
+    System.out.println(d);
+	}
+
+}
